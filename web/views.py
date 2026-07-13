e0529945030e9783688d820b318e5d0adf0b6d7a
siwadol rukchart <hikiyasabuno@gmail.com>
2026-07-13 15:03:49 +0700

    yeah

--- web/views.py
+++ web/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,HttpResponse
 from django.http import HttpResponse
 # Create your views here.
 def index (request) :
-    return HttpResponse("index")
+    return render(request,'index.html')
 def about (request) :
     return HttpResponse("about")
 def contact (request) :
